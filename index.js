bc830dc99ca9f1fe5cde320d0e85e97aace1a6f1
SEYIDev <seyikabiru@gmail.com>
2025-02-19 13:42:45 -0800

    new

--- index.js
+++ index.js
@@ -123,3 +123,22 @@ function generateAllPDF() {
     doc.save("All_Registrations.pdf"); // Save as PDF
 }
 
+
+// Function to display carousel
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
