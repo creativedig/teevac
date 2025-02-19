84fc0b2a3db5b076ff1fc50dbc1eb01754ee7300
SEYIDev <seyikabiru@gmail.com>
2025-02-19 13:47:10 -0800

    Comment

--- index.js
+++ index.js
@@ -126,19 +126,4 @@ function generateAllPDF() {
 
 // Function to display carousel
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+
