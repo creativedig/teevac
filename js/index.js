bd0d58214088eebdf641dd1e01eaeadb28260392
SEYIDev <seyikabiru@gmail.com>
2025-03-05 23:16:41 -0800

    cHANGES

--- js/index.js
+++ js/index.js
@@ -163,3 +163,20 @@ document.addEventListener("DOMContentLoaded", function () {
         observer.observe(box);
     });
 });
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let elements = document.querySelectorAll(".inner-virtual-box");
+    let observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("fade-in");
+        }
+      });
+    }, { threshold: 0.3 });
+    
+    elements.forEach(el => observer.observe(el));
+});
